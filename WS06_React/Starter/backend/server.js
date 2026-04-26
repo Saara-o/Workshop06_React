c292f3a3a864e08215b8fa201ceb194e645b5ee0
Saara <saara.orvas@student.laurea.fi>
2026-04-26 23:34:55 +0300

    Cleaned server.js

--- WS06_React/Starter/backend/server.js
+++ WS06_React/Starter/backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const pagesRouter = require('./routes/pages');
 const postsRouter = require('./routes/posts');
 
 const app = express();
@@ -29,23 +28,22 @@ app.locals.publicDir = publicDir;
 app.use(express.json());
 app.use(express.static(publicDir));
 
-app.use('/', pagesRouter);
 app.use('/api/posts', postsRouter);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(publicDir, '404.html'));
+  res.status(404).send('Not found');
 });
 
 app.use((error, req, res, next) => {
   console.error(error.stack);
-  res.status(500).sendFile(path.join(publicDir, '500.html'));
+  res.status(500).send('Server error');
 });
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log('Mounted routers:');
-    console.log('  / -> routes/pages.js');
+    //console.log('  / -> routes/pages.js');
     console.log('  /api/posts -> routes/posts.js');
   });
 });
