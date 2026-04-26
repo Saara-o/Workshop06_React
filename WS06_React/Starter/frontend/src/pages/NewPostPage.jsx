47de1f23282896dac589b9eb6631215c19ba1310
Saara <saara.orvas@student.laurea.fi>
2026-04-26 23:20:48 +0300

    Created a new post using handleSubmit and POST method

--- WS06_React/Starter/frontend/src/pages/NewPostPage.jsx
+++ WS06_React/Starter/frontend/src/pages/NewPostPage.jsx
@@ -2,12 +2,8 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import PostForm from '../components/PostForm.jsx'
 
-// TODO (student): Implement create flow (POST /api/posts).
-// Suggested steps:
-// 1) Read form values in handleSubmit.
-// 2) POST JSON body to /api/posts.
-// 3) On success, navigate to /posts/:id.
-// 4) Show an error message on failure.
+// Implement create flow (POST /api/posts).
+
 function NewPostPage() {
   const navigate = useNavigate()
   const [submitting, setSubmitting] = useState(false)
@@ -19,8 +15,31 @@ function NewPostPage() {
     setError(null)
 
     // TODO (student): Implement this submit logic.
-    setError('TODO: implement POST /api/posts in NewPostPage')
-    setSubmitting(false)
+    // 1) Read form values in handleSubmit.
+    const form = e.target
+    const title = form.title.value
+    const author = form.author.value
+    const content = form.content.value
+
+    // 2) POST JSON body to /api/posts.
+    try {
+      const res = await fetch('/api/posts', {
+        method: 'POST',
+        headers: {'Accept' : 'application/json', 'Content-Type' : 'application/json',},
+        body: JSON.stringify({title, author, content})
+      })
+      // 4) Show an error message on failure.
+      if (!res.ok) {
+        throw new Error('Failed to create a new post')
+      }
+      // 3) On success, navigate to /posts/:id.
+      const data = await res.json()
+      navigate(`/posts/${data._id}`)
+
+    } catch (error) {
+      setError(error.message)
+      setSubmitting(false)
+    }
   }
 
   return (
